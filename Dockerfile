0d44c9c6d54b4a201e54bd7c6994d0d5ebc38061
Max <amax0204@gmail.com>
2023-04-10 01:45:00 +0300

    docker, supervisor, tests

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
-EXPOSE 8090
+EXPOSE 8888
+# EXPOSE $PORT
 
 COPY requirements.txt /app/requirements.txt
 
@@ -13,6 +14,4 @@ RUN pip install --no-cache-dir -r /app/requirements.txt
 
 COPY . /app
 
-# CMD ["python", "worker.py", "&"]
-CMD ["python", "app.py"]
-# CMD ["sh", "-c", "python app.py & python news_parser.py"]
+CMD ["python", "$APP"]
---
+++ tests/use_cases/test_article.py
@@ -0,0 +1,42 @@
+import pytest
+from unittest.mock import Mock
+from news.responses import (
+    ResponseSuccess,
+    ResponseFailure,
+    ResponseTypes,
+)
+from news.domain.article import Article
+from news.use_cases.article import article_count_use_case
+from news.repository.mongo import MongoRepo
+
+@pytest.fixture
+def mock_repo():
+    repo = Mock()
+    return repo
+
+
+@pytest.mark.asyncio
+async def test_article_count_use_case_valid(mock_repo):
+    mock_repo.count_documents.return_value = 10
+
+    response = await article_count_use_case(mock_repo)
+
+    yield
+
+    print(response)
+    assert bool(response) is True
+    assert response.value == 10
+
+
+@pytest.mark.asyncio
+async def test_article_count_use_case_exception(mock_repo):
+    mock_repo.count_documents.side_effect = Exception("Just an error message")
+
+    response = await article_count_use_case(mock_repo)
+
+    assert bool(response) is False
+    assert response.value == {
+        "type": ResponseTypes.SYSTEM_ERROR,
+        "message": "Exception: Just an error message",
+    }
+
